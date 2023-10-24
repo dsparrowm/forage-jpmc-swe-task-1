6a8b97294d93ba7767aa07ca3a8fb10404c5bcf2
Davies Aniefiok <daviesaniefiok32@gmail.com>
2023-10-24 00:49:16 -0700

    I made changes to getRatio method in client3.py to ensure it returns the proper ratio. I also modified getDataPoint method in client3.py to properly calculate and return the price

--- client_test.py
+++ client_test.py
@@ -1,5 +1,5 @@
 import unittest
-from client3 import getDataPoint
+from client3 import getDataPoint, getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -7,7 +7,10 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
+    price = ((quotes[0]['top_bid']['price'] + quotes[0]['top_ask']['price']) / 2)
     """ ------------ Add the assertion below ------------ """
+    expected_price = getDataPoint(quotes[0])
+    self.assertEqual(expected_price[3], price)
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,9 +18,20 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
-
+    greater = getDataPoint(quotes[0])
+    self.assertTrue(greater[1] > greater[2])
 
   """ ------------ Add more unit tests ------------ """
+  def test_getRatio_checkPrice_bIsZero(self):
+     self.assertIsNone(getRatio(10, 0))
+
+  def test_getRatio_ProperRatio(self):
+     quotes = [
+      {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+    ]
+     expected_ratio = quotes[0]['top_bid']['price'] / quotes[0]['top_ask']['price']
+     self.assertEqual(getRatio(quotes[0]['top_bid']['price'], quotes[0]['top_ask']['price']), expected_ratio)
 
 
 
